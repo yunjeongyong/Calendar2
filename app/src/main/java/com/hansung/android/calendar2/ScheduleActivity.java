9e0f83880195d4130a5f7f48111777aa41c49f20
yunjeongyong <dbswjd9698@hansung.ac.kr>
2021-05-30 20:30:50 +0900

    2021.05.30 20:30

--- app/src/main/java/com/hansung/android/calendar2/ScheduleActivity.java
+++ app/src/main/java/com/hansung/android/calendar2/ScheduleActivity.java
@@ -3,6 +3,8 @@ package com.hansung.android.calendar2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -21,9 +23,9 @@ public class ScheduleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
-        Intent intent = new Intent();
+        Intent intent = getIntent();
         year = intent.getIntExtra("year", -1);
-        month = intent.getIntExtra("month", -1);
+        month = intent.getIntExtra("month", -1) + 1;
         date = intent.getIntExtra("date", -1);
         hour = intent.getIntExtra("hour", -1);
         endHour = (hour + 1) % 24;
@@ -41,5 +43,33 @@ public class ScheduleActivity extends Activity {
 
         TimePicker endPicker = findViewById(R.id.schedule_end);
         endPicker.setCurrentHour(endHour);
+
+        Button saveButton = findViewById(R.id.schedule_save_btn);
+        Button cancelButton = findViewById(R.id.schedule_cancel_btn);
+        Button delButton = findViewById(R.id.schedule_del_btn);
+
+        // 저장 버튼 누르면
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 저장할 때 어떻게 해야 되는지
+            }
+        });
+
+        //취소 버튼 누르면
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishActivity(0);
+            }
+        });
+
+        // 삭제 버튼 누르면
+        delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 스케줄 삭제할 때
+            }
+        });
     }
 }
